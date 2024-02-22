7cb90731c9d27722fca7520a27264587a0502025
Mason Redacted <67813185+254nm@users.noreply.github.com>
2024-02-21 23:31:31 -0500

    Fix CCE

--- src/main/java/me/txmc/core/antiillegal/check/checks/NameCheck.java
+++ src/main/java/me/txmc/core/antiillegal/check/checks/NameCheck.java
@@ -1,13 +1,14 @@
 package me.txmc.core.antiillegal.check.checks;
 
 import lombok.RequiredArgsConstructor;
-import me.txmc.core.antiillegal.check.Check;import net.kyori.adventure.text.Component;
+import me.txmc.core.antiillegal.check.Check;
+import me.txmc.core.util.GlobalUtils;
+import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextComponent;
 import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
-
 /**
  * @author 254n_m
  * @since 2023/09/20 8:01 PM
@@ -21,10 +22,10 @@ public class NameCheck implements Check {
         if (!item.hasItemMeta()) return false;
         ItemMeta meta = item.getItemMeta();
         if (meta.displayName() == null) return false;
-        TextComponent name = (TextComponent) meta.displayName();
+        Component name =  meta.displayName();
         if (name.hasStyling()) return true;
         if (hasDecorations(name)) return true;
-        if (name.content().length() > 50) return true;
+        if (GlobalUtils.getStringContent(name).length() > 50) return true;
         return false;
     }
 
@@ -36,13 +37,13 @@ public class NameCheck implements Check {
     @Override
     public void fix(ItemStack item) {
         ItemMeta meta = item.getItemMeta();
-        TextComponent name = (TextComponent) meta.displayName();
-        String strName = name.content();
+        Component name = meta.displayName();
+        String strName = GlobalUtils.getStringContent(name);
         meta.displayName(Component.text(strName.substring(0, Math.min(config.getInt("MaxItemNameLength"), strName.length()))));
         item.setItemMeta(meta);
     }
 
-    private boolean hasDecorations(TextComponent component) {
+    private boolean hasDecorations(Component component) {
         for (TextDecoration decoration : TextDecoration.values()) {
             if (component.hasDecoration(decoration)) return true;
         }
