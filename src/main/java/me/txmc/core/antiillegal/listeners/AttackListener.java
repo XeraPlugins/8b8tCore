e2937bf5618317f6475dd420d6327881c12dd0c8
natelegault <nate@xera.ca>
2024-07-10 20:16:18 -0600

    added book ban patch

--- src/main/java/me/txmc/core/antiillegal/listeners/AttackListener.java
+++ src/main/java/me/txmc/core/antiillegal/listeners/AttackListener.java
@@ -1,9 +1,17 @@
 package me.txmc.core.antiillegal.listeners;
 
+import org.bukkit.Material;
+import org.bukkit.block.ShulkerBox;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.BlockStateMeta;
+
+import static org.apache.logging.log4j.LogManager.getLogger;
 
 /**
  * @author 254n_m
@@ -19,4 +27,37 @@ public class AttackListener implements Listener {
             ((Player) event.getDamager()).damage(event.getDamage());
         }
     }
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        Player player = event.getPlayer();
+        getLogger().info("PlayerQuitEvent triggered for player: " + player.getName());
+
+        // Iterate through the player's inventory
+        for (ItemStack item : player.getInventory().getContents()) {
+            getLogger().info("" + item.getType().toString());
+            if (item != null && item.getType().toString().endsWith("SHULKER_BOX")) {
+                getLogger().info("tiene shulkers");
+                BlockStateMeta blockStateMeta = (BlockStateMeta) item.getItemMeta();
+                if (blockStateMeta != null && blockStateMeta.getBlockState() instanceof ShulkerBox) {
+                    ShulkerBox shulkerBox = (ShulkerBox) blockStateMeta.getBlockState();
+                    Inventory shulkerInventory = shulkerBox.getInventory();
+
+                    boolean containsBooks = false;
+                    for (ItemStack shulkerItem : shulkerInventory.getContents()) {
+                        if (shulkerItem != null && shulkerItem.getType() == Material.WRITTEN_BOOK) {
+                            containsBooks = true;
+                            break;
+                        }
+                    }
+
+                    if (containsBooks) {
+                        shulkerInventory.clear();
+                        blockStateMeta.setBlockState(shulkerBox);
+                        item.setItemMeta(blockStateMeta);
+                        getLogger().info("Cleared Shulker Box containing books in " + player.getName() + "'s inventory.");
+                    }
+                }
+            }
+    }
+}
 }
