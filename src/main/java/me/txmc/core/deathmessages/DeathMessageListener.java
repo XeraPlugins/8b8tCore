b927b07c56db6616d0b49b5ddb62113b14d88043
Minelord9000 <alex.garcia.corte@gmail.com>
2024-07-21 00:28:00 -0600

    TPS Command Added

--- src/main/java/me/txmc/core/deathmessages/DeathMessageListener.java
+++ src/main/java/me/txmc/core/deathmessages/DeathMessageListener.java
@@ -42,7 +42,6 @@ public class DeathMessageListener implements Listener {
         Entity killer = victim.getKiller();
         ItemStack weapon = null;
 
-
         if (victim.getLastDamageCause() == null) {
             return;
         }
