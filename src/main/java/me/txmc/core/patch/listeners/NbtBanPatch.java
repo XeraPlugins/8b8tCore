71b1f3bcb1af0e6ba527f271a6934d2c1cefcc89
Alejandro Garcia <biz.alejandrogarcia@gmail.com>
2024-08-04 19:01:32 -0600

    NBTPatch now checks all shulkers once player join

--- src/main/java/me/txmc/core/patch/listeners/NbtBanPatch.java
+++ src/main/java/me/txmc/core/patch/listeners/NbtBanPatch.java
@@ -5,25 +5,27 @@ import org.bukkit.block.ShulkerBox;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.BlockStateMeta;
+import org.bukkit.inventory.meta.ItemMeta;
 
 import java.nio.charset.StandardCharsets;
 
+import static me.txmc.core.util.GlobalUtils.sendPrefixedLocalizedMessage;
 import static org.apache.logging.log4j.LogManager.getLogger;
 
 /**
  * Listener for handling the NBT (Named Binary Tag) data of shulker boxes in the player's inventory.
  *
  * <p>This class is part of the 8b8tCore plugin and is responsible for ensuring that shulker boxes
- * containing excessive amounts of data are cleared from the player's inventory upon logout. It
- * particularly focuses on shulker boxes that contain written books and exceed a specified size.</p>
+ * containing excessive amounts of data are cleared from the player's inventory upon join.</p>
  *
  * <p>Functionality includes:</p>
  * <ul>
- *     <li>Detecting shulker boxes in the player's inventory upon logout</li>
+ *     <li>Detecting shulker boxes in the player's inventory upon join</li>
  *     <li>Checking if the shulker box contains written books</li>
  *     <li>Calculating the size of the shulker box's metadata</li>
  *     <li>Clearing the contents of shulker boxes that exceed a specific size threshold</li>
@@ -35,7 +37,7 @@ import static org.apache.logging.log4j.LogManager.getLogger;
 public class NbtBanPatch implements Listener {
 
     @EventHandler
-    public void onPlayerQuit(PlayerQuitEvent event) {
+    public void onPlayerJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
 
         // Iterate through the player's inventory
@@ -50,28 +52,20 @@ public class NbtBanPatch implements Listener {
                     ShulkerBox shulkerBox = (ShulkerBox) blockStateMeta.getBlockState();
                     Inventory shulkerInventory = shulkerBox.getInventory();
 
-                    boolean containsBooks = false;
-
-                    for (ItemStack shulkerItem : shulkerInventory.getContents()) {
-                        // Check if the shulker has written books
-                        if (shulkerItem != null && shulkerItem.getType() == Material.WRITTEN_BOOK) {
-                            containsBooks = true;
-                            break;
-                        }
-                    }
-
 
                     // The shulker's metadata is converted to a string, and then the size of the string in bytes is calculated
                     int shulkerSize = calculateStringSizeInBytes(item.getItemMeta().toString());
-                    //getLogger().info("Books Shulker size: " + shulkerSize + " bytes => " + player.getName() + "'s inventory.");
 
                     // Check if the shulker exceeds a specific number of bytes, if so, its contents are removed
-                    if (containsBooks && shulkerSize > 60000) { // Max shulker size in bytes calculated based on tests
+                    if (shulkerSize > 65000) { // Max shulker size in bytes calculated based on tests
                         shulkerInventory.clear();
                         blockStateMeta.setBlockState(shulkerBox);
                         item.setItemMeta(blockStateMeta);
-                        getLogger().info("Cleared Shulker Box containing too large books in " + player.getName() + "'s inventory with size: " + shulkerSize);
+                        getLogger().info("Cleared Shulker Box in " + player.getName() + "'s inventory with size " + shulkerSize + "bytes named '" + getShulkerName(item) + "'");
                     }
+
+                    sendPrefixedLocalizedMessage(player, "nbtPatch_deleted_item",getShulkerName(item));
+
                 }
             }
         }
@@ -81,5 +75,19 @@ public class NbtBanPatch implements Listener {
     private int calculateStringSizeInBytes(String data)  {
         byte[] byteArray = data.getBytes(StandardCharsets.UTF_8);
         return byteArray.length;
+    }
+
+    private String getShulkerName(ItemStack itemStack) {
+        if (itemStack == null) {
+            return "";
+        }
+
+        ItemMeta meta = itemStack.getItemMeta();
+
+        if (meta != null && meta.hasDisplayName()) {
+            return String.valueOf(itemStack.getItemMeta().getDisplayName());
+        } else {
+            return itemStack.getType().toString().replace("_", " ").toLowerCase();
+        }
     }
 }
