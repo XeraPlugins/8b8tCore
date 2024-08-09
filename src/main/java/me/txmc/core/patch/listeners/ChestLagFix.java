a01f78c20c4b2afa4db2600a5f7503aad9e149e3
natelegault <nate@xera.ca>
2024-08-09 15:55:23 -0600

    1.1.4 Chest Lag Fix Changes

--- src/main/java/me/txmc/core/patch/listeners/ChestLagFix.java
+++ src/main/java/me/txmc/core/patch/listeners/ChestLagFix.java
@@ -1,10 +1,6 @@
 package me.txmc.core.patch.listeners;
 
-import me.txmc.core.chat.tasks.AnnouncementTask;
-import me.txmc.core.tablist.util.Utils;
 import me.txmc.core.util.GlobalUtils;
-import me.txmc.core.util.SecondPassEvent;
-import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.block.ShulkerBox;
 import org.bukkit.entity.Player;
@@ -16,17 +12,9 @@ import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.BlockStateMeta;
 import org.bukkit.inventory.meta.BookMeta;
-import org.bukkit.plugin.Plugin;
-import org.bukkit.scheduler.BukkitRunnable;
-import org.bukkit.scheduler.BukkitScheduler;
 import  me.txmc.core.Main;
-import org.bukkit.scheduler.BukkitTask;
 
 import java.util.HashMap;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
-import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -40,7 +28,7 @@ public class ChestLagFix implements Listener {
     }
 
     @EventHandler
-    public void onInventoryOpen(InventoryOpenEvent event) {
+    public void onInventoryOpen2(InventoryOpenEvent event) {
         try {
             InventoryType inventoryType = event.getInventory().getType();
             Player player = (Player) event.getPlayer();
@@ -50,27 +38,23 @@ public class ChestLagFix implements Listener {
                 } else {
                     chestHashMap.put(player, 1);
                 }
-                deleteNBTBooks(event.getInventory());
                 if (chestHashMap.get(player) > 5) {
                     ((Player) event.getPlayer()).kickPlayer("Chest lag exploit has been patched");
                     chestHashMap.remove(player);
+                    deleteNBTBooks(event.getInventory());
                 }
             }
         } catch (IllegalArgumentException e) {
             e.printStackTrace();
         }
+        Player player = (Player) event.getPlayer();
         GlobalUtils.getRegionTps(event.getPlayer().getLocation()).thenAccept(tps -> {
-            if (tps >= 17) {
-                chestHashMap.clear();
+            if (tps >= 18) {
+                if (chestHashMap != null) {
+                    chestHashMap.clear();
+                }
             }});
     }
-
-
-    @EventHandler
-    public void onSecondPass(SecondPassEvent event) {
-        Utils.secondPass(chestHashMap);
-    }
-
     public boolean isCheckedInventory(InventoryType type) {
         switch (type) {
             case CHEST:
