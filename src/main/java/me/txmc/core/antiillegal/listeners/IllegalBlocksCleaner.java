d2bbfae9f155c5ca5eb2b776575ac239ea830e49
Alejandro Garcia <biz.alejandrogarcia@gmail.com>
2024-10-06 21:46:42 -0600

    Illegal Blocks Cleaner fixed

--- src/main/java/me/txmc/core/antiillegal/listeners/IllegalBlocksCleaner.java
+++ src/main/java/me/txmc/core/antiillegal/listeners/IllegalBlocksCleaner.java
@@ -37,9 +37,9 @@ public class IllegalBlocksCleaner implements Listener {
         Chunk chunk = event.getChunk();
 
         int yUpperLimit = event.getWorld().getMaxHeight();
-        int yLowerLimit = event.getWorld().getMinHeight() + 5;
+        int yLowerLimit = event.getWorld().getMinHeight();
 
-        if(event.getWorld().getEnvironment() == World.Environment.NETHER){
+        if (event.getWorld().getEnvironment() == World.Environment.NETHER) {
             yUpperLimit = 125;
         }
 
@@ -47,17 +47,19 @@ public class IllegalBlocksCleaner implements Listener {
             for (int z = 0; z < 16; z++) {
                 for (int y = yLowerLimit; y < yUpperLimit; y++) {
                     Block block = chunk.getBlock(x, y, z);
-                    if (
-                            block.getType() == Material.BEDROCK ||
-                            block.getType() == Material.END_PORTAL_FRAME ||
-                            block.getType() == Material.REINFORCED_DEEPSLATE ||
-                            block.getType() == Material.BARRIER ||
-                            block.getType() == Material.LIGHT
-                    ) {
+                    Material type = block.getType();
+
+                    if (type == Material.END_PORTAL_FRAME ||
+                            type == Material.REINFORCED_DEEPSLATE ||
+                            type == Material.BARRIER ||
+                            type == Material.LIGHT ||
+                            type == Material.END_PORTAL ||
+                            (type == Material.BEDROCK && y >= yLowerLimit + 5)) {
                         block.setType(Material.AIR);
                     }
                 }
             }
         }
     }
+
 }
