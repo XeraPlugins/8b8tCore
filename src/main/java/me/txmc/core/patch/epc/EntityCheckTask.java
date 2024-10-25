1b71c4c6751459049bf3e700ce80692e7d0bbb80
Alejandro Garcia <biz.alejandrogarcia@gmail.com>
2024-10-24 22:36:29 -0600

    temporal patch EntityChecker

--- src/main/java/me/txmc/core/patch/epc/EntityCheckTask.java
+++ src/main/java/me/txmc/core/patch/epc/EntityCheckTask.java
@@ -18,21 +18,29 @@ public class EntityCheckTask implements Runnable {
 
     @Override
     public void run() {
-        for (Chunk[] chunks : Bukkit.getWorlds().stream().map(World::getLoadedChunks).toList()) {
-            for (Chunk chunk : chunks) {
-                if (chunk.getEntities().length == 0) continue;
-                main.entityPerChunk().forEach((e, i) -> {
-                    Entity[] entities = Arrays.stream(chunk.getEntities()).filter(en -> en.getType() == e).toList().toArray(Entity[]::new);
-                    int amt = entities.length;
-                    if (amt >= i) {
-                        log(Level.INFO, "Removed %d entities from chunk %d,%d in world %s", amt-i, chunk.getX(), chunk.getZ(), chunk.getWorld().getName());
-                        for (int j = 0; j < amt-i; j++) {
-                            Entity entity = entities[j];
-                            entity.getScheduler().run(main.plugin(), (t) -> entity.remove(), () -> {});
+        try {
+            for (Chunk[] chunks : Bukkit.getWorlds().stream().map(World::getLoadedChunks).toList()) {
+                for (Chunk chunk : chunks) {
+                    if (chunk.getEntities().length == 0) continue;
+                    main.entityPerChunk().forEach((e, i) -> {
+                        Entity[] entities = Arrays.stream(chunk.getEntities())
+                                .filter(en -> en.getType() == e)
+                                .toList()
+                                .toArray(Entity[]::new);
+                        int amt = entities.length;
+                        if (amt >= i) {
+                            log(Level.INFO, "Removed %d entities from chunk %d,%d in world %s", amt - i, chunk.getX(), chunk.getZ(), chunk.getWorld().getName());
+                            for (int j = 0; j < amt - i; j++) {
+                                Entity entity = entities[j];
+                                entity.getScheduler().run(main.plugin(), (t) -> entity.remove(), () -> {});
+                            }
                         }
-                    }
-                });
+                    });
+                }
             }
+        } catch (Exception ex) {
+            log(Level.SEVERE, "An error occurred while checking entities: %s", ex.getMessage());
+            ex.printStackTrace();
         }
     }
 }
