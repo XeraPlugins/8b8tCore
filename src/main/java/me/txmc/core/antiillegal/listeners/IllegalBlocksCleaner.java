c7b1ac25332516ef5db301d84567d0d512543620
agarciacorte <alex.garcia.corte@gmail.com>
2025-02-21 22:11:47 -0600

    Illegal blocks cleaner fix

--- src/main/java/me/txmc/core/antiillegal/listeners/IllegalBlocksCleaner.java
+++ src/main/java/me/txmc/core/antiillegal/listeners/IllegalBlocksCleaner.java
@@ -1,5 +1,6 @@
 package me.txmc.core.antiillegal.listeners;
 
+import lombok.extern.slf4j.Slf4j;
 import org.bukkit.Chunk;
 import org.bukkit.Material;
 import org.bukkit.World;
@@ -7,6 +8,8 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.world.ChunkLoadEvent;
 import org.bukkit.block.Block;
+
+import static java.lang.Math.abs;
 
 /**
  * Listener for cleaning illegal blocks in chunks when they are loaded.
@@ -29,18 +32,25 @@ import org.bukkit.block.Block;
  * @since 2024/08/06 23:30
  */
 
+@Slf4j
 public class IllegalBlocksCleaner implements Listener {
 
     @EventHandler
     public void onChunkLoad(ChunkLoadEvent event) {
 
+        if(event.isNewChunk()) return;
+
         Chunk chunk = event.getChunk();
+        if(abs(chunk.getX()) >= 1000000 || abs(chunk.getZ()) >= 1000000) return;
 
         int yUpperLimit = event.getWorld().getMaxHeight();
+
         int yLowerLimit = event.getWorld().getMinHeight();
 
-        if (event.getWorld().getEnvironment() == World.Environment.NETHER) {
-            yUpperLimit = 125;
+        World.Environment worldEnv = event.getWorld().getEnvironment();
+
+        if (worldEnv == World.Environment.NETHER) {
+            yUpperLimit = 127;
         }
 
         for (int x = 0; x < 16; x++) {
@@ -49,17 +59,40 @@ public class IllegalBlocksCleaner implements Listener {
                     Block block = chunk.getBlock(x, y, z);
                     Material type = block.getType();
 
-                    if (type == Material.END_PORTAL_FRAME ||
+                    // NETHER
+                    if (worldEnv == World.Environment.NETHER && (
+                            type == Material.END_PORTAL_FRAME ||
                             type == Material.REINFORCED_DEEPSLATE ||
                             type == Material.BARRIER ||
                             type == Material.LIGHT ||
                             type == Material.END_PORTAL ||
-                            (type == Material.BEDROCK && y >= yLowerLimit + 5)) {
+                            (type == Material.BEDROCK && y >= yLowerLimit + 5 && y <= yUpperLimit - 5))) {
+                        block.setType(Material.AIR);
+                    }
+
+                    // OVERWORLD
+                    if (worldEnv == World.Environment.NORMAL && (
+                            type == Material.END_PORTAL_FRAME ||
+                            type == Material.REINFORCED_DEEPSLATE ||
+                            type == Material.BARRIER ||
+                            type == Material.LIGHT ||
+                            type == Material.END_PORTAL ||
+                            (type == Material.BEDROCK && y >= yLowerLimit + 5))) {
+                        block.setType(Material.AIR);
+                    }
+
+                    // END
+                    if (worldEnv == World.Environment.THE_END && (
+                            type == Material.END_PORTAL_FRAME ||
+                            type == Material.REINFORCED_DEEPSLATE ||
+                            type == Material.BARRIER ||
+                            type == Material.LIGHT ||
+                            type == Material.END_PORTAL ||
+                            (type == Material.BEDROCK))) {
                         block.setType(Material.AIR);
                     }
                 }
             }
         }
     }
-
 }
