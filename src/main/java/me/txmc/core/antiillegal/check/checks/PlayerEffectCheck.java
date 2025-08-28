1f8d8569c6b86ce99e5ab48240b6531ea39d01af
libalpm64 <159192189+libalpm64@users.noreply.github.com>
2025-08-28 08:18:22 -0400

    Update PlayerEffectCheck.java

--- src/main/java/me/txmc/core/antiillegal/check/checks/PlayerEffectCheck.java
+++ src/main/java/me/txmc/core/antiillegal/check/checks/PlayerEffectCheck.java
@@ -27,7 +27,7 @@ public class PlayerEffectCheck implements Check {
         // Standard potions (max level 1)
         VANILLA_LEVEL_LIMITS.put(PotionEffectType.NIGHT_VISION, 1);
         VANILLA_LEVEL_LIMITS.put(PotionEffectType.INVISIBILITY, 1);
-        VANILLA_LEVEL_LIMITS.put(PotionEffectType.LUCK, 1);
+
         VANILLA_LEVEL_LIMITS.put(PotionEffectType.SLOW_FALLING, 1);
         VANILLA_LEVEL_LIMITS.put(PotionEffectType.WATER_BREATHING, 1);
         VANILLA_LEVEL_LIMITS.put(PotionEffectType.FIRE_RESISTANCE, 1);
@@ -39,6 +39,12 @@ public class PlayerEffectCheck implements Check {
         VANILLA_LEVEL_LIMITS.put(PotionEffectType.GLOWING, 1);
         VANILLA_LEVEL_LIMITS.put(PotionEffectType.NAUSEA, 1);
         VANILLA_LEVEL_LIMITS.put(PotionEffectType.BLINDNESS, 1);
+        VANILLA_LEVEL_LIMITS.put(PotionEffectType.WIND_CHARGED, 1); // Windcharge weapon
+        VANILLA_LEVEL_LIMITS.put(PotionEffectType.INFESTED, 1); // Windcharge weapon
+        VANILLA_LEVEL_LIMITS.put(PotionEffectType.SATURATION, 1); 
+        VANILLA_LEVEL_LIMITS.put(PotionEffectType.TRIAL_OMEN, 1); // Trial Chamber
+        VANILLA_LEVEL_LIMITS.put(PotionEffectType.WEAVING, 1); // Creates cobwebs on death
+
 
         /*
         // Bedrock (Not in the Bukkit API as it's only for Java)
@@ -54,6 +60,9 @@ public class PlayerEffectCheck implements Check {
         // VANILLA_LEVEL_LIMITS.put(PotionEffectType.SMALL, 2);
         // VANILLA_LEVEL_LIMITS.put(PotionEffectType.CARING, 2);
         // VANILLA_LEVEL_LIMITS.put(PotionEffectType.SHARING, 1);
+        // Luck Effects (Commented out)
+        // VANILLA_LEVEL_LIMITS.put(PotionEffectType.LUCK, 0);
+        // VANILLA_LEVEL_LIMITS.put(PotionEffectType.UNLUCK, 0);
         */
 
         // Potions with max level 2
@@ -65,22 +74,15 @@ public class PlayerEffectCheck implements Check {
         VANILLA_LEVEL_LIMITS.put(PotionEffectType.HASTE, 2);
         VANILLA_LEVEL_LIMITS.put(PotionEffectType.INSTANT_DAMAGE, 2);
         VANILLA_LEVEL_LIMITS.put(PotionEffectType.INSTANT_HEALTH, 2);
-
+        VANILLA_LEVEL_LIMITS.put(PotionEffectType.WITHER, 2);
+        VANILLA_LEVEL_LIMITS.put(PotionEffectType.LEVITATION, 2);
+        
         // Potions with max level 4
         VANILLA_LEVEL_LIMITS.put(PotionEffectType.SLOWNESS, 4);
         VANILLA_LEVEL_LIMITS.put(PotionEffectType.RESISTANCE, 4);
         VANILLA_LEVEL_LIMITS.put(PotionEffectType.ABSORPTION, 4);
 
-
         // Special cases
-        VANILLA_LEVEL_LIMITS.put(PotionEffectType.WIND_CHARGED, 0); // Windcharge weapon
-        VANILLA_LEVEL_LIMITS.put(PotionEffectType.INFESTED, 0); // Windcharge weapon
-        VANILLA_LEVEL_LIMITS.put(PotionEffectType.TRIAL_OMEN, 0); // Trial Chamber
-        VANILLA_LEVEL_LIMITS.put(PotionEffectType.WEAVING, 0); // Creates cobwebs on death
-        VANILLA_LEVEL_LIMITS.put(PotionEffectType.WITHER, 2); // Wither Boss - Potion of Decay
-        VANILLA_LEVEL_LIMITS.put(PotionEffectType.SATURATION, 0); // Only via Golden Apples
-        VANILLA_LEVEL_LIMITS.put(PotionEffectType.UNLUCK, 0); // Only via commands
-        VANILLA_LEVEL_LIMITS.put(PotionEffectType.LEVITATION, 2); // Shulkers 
         VANILLA_LEVEL_LIMITS.put(PotionEffectType.MINING_FATIGUE, 3); // Elder Guardian
         VANILLA_LEVEL_LIMITS.put(PotionEffectType.HUNGER, 3); // Pufferfish
         VANILLA_LEVEL_LIMITS.put(PotionEffectType.HERO_OF_THE_VILLAGE, 5); // Village curing
