ff586bc698499e2ba807f16f9c0a2e11e3b14e13
libalpm64 <159192189+libalpm64@users.noreply.github.com>
2025-09-17 00:53:44 -0400

    Update IllegalDataCheck.java

--- src/main/java/me/txmc/core/antiillegal/check/checks/IllegalDataCheck.java
+++ src/main/java/me/txmc/core/antiillegal/check/checks/IllegalDataCheck.java
@@ -1,25 +1,75 @@
 package me.txmc.core.antiillegal.check.checks;
 
-import me.txmc.core.antiillegal.check.Check;import org.bukkit.inventory.ItemStack;
+import io.papermc.paper.datacomponent.item.DeathProtection;
+import me.txmc.core.antiillegal.check.Check;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 /**
- * @author 254n_m
- * @since 2023/09/29 6:53 PM
+ * @author MindComplexity
+ * @since 2025/09/17
  * This file was created as a part of 8b8tAntiIllegal
  */
+
 public class IllegalDataCheck implements Check {
+    private static final int MAX_LEGAL_AMPLIFIER = 5;
+    private static final int MAX_LEGAL_DURATION = 9600;
+
     @Override
     public boolean check(ItemStack item) {
+        if (item == null || !item.hasItemMeta()) return false;
+        
+        ItemMeta meta = item.getItemMeta();
+        if (meta == null) return false;
+        
+        try {
+            if (meta.isGlider() && item.getType() != Material.ELYTRA) {
+                return true;
+            }
+            
+            if (hasDeathProtectionComponent(meta) && item.getType() != Material.TOTEM_OF_UNDYING) {
+                return true;
+            }
+
+            if (hasIllegalFoodEffects(meta)) {
+                return true;
+            }
+            
+        } catch (Exception e) {
+            return false;
+        }
+        
         return false;
     }
 
     @Override
     public boolean shouldCheck(ItemStack item) {
-        return false;
+        return item != null && item.hasItemMeta();
     }
 
     @Override
     public void fix(ItemStack item) {
-
+        item.setAmount(0);
+    }
+    
+    private boolean hasDeathProtectionComponent(ItemMeta meta) {
+        String componentString = meta.getAsComponentString();
+        return componentString.contains("minecraft:death_protection");
+    }
+    
+    private boolean hasIllegalFoodEffects(ItemMeta meta) {
+        String componentString = meta.getAsComponentString().toLowerCase();
+        
+        if (componentString.contains("minecraft:food") || componentString.contains("minecraft:consumable")) {
+            if (componentString.contains("amplifier") && componentString.matches(".*amplifier[=\\s]+([6-9]|\\d{2,}).*")) {
+                return true;
+            }
+            
+            if (componentString.contains("duration") && componentString.matches(".*duration[=\\s]+(9[7-9][0-9]{2}|[1-9]\\d{5,}).*")) {
+                return true;
+            }
+        }        
+        return false;
     }
 }
