e988890f5d861e676684576e2d5ea6df48575512
nate <nate@xera.ca>
2025-09-23 20:25:08 -0600

    1.6.4

--- src/main/java/me/txmc/core/customexperience/util/PrefixManager.java
+++ src/main/java/me/txmc/core/customexperience/util/PrefixManager.java
@@ -25,18 +25,24 @@ public class PrefixManager {
             "8b8tcore.prefix.owner",
             "8b8tcore.prefix.dev",
             "8b8tcore.prefix.bot",
+            "8b8tcore.prefix.youtuber",
+            "8b8tcore.prefix.thetroll2001",
             "8b8tcore.prefix.donator6",
             "8b8tcore.prefix.donator5",
             "8b8tcore.prefix.donator4",
             "8b8tcore.prefix.donator3",
             "8b8tcore.prefix.donator2",
             "8b8tcore.prefix.donator1"
+
+
     );
 
     static {
         PREFIXES.put("8b8tcore.prefix.owner", "<gradient:#BA3FFC:#D9ADFD:#BA3FFC:%s>[OWNER<green>✔</green>]</gradient>");
         PREFIXES.put("8b8tcore.prefix.dev", "<gradient:#5555FF:#9492F5:#5555FF:%s>[DEV<green>✔</green>]</gradient>");
         PREFIXES.put("8b8tcore.prefix.bot", "<gradient:#00AA00:#6ef385:#00AA00:%s>[BOT]</gradient>");
+        PREFIXES.put("8b8tcore.prefix.youtuber", "<gradient:#8B0000:#FF0000:#8B0000:%s>[Youtuber]</gradient>");
+        PREFIXES.put("8b8tcore.prefix.thetroll2001", "<gradient:#FF0000:#FF7F00:#FFFF00:#00FF00:#0000FF:#4B0082:#8F00FF:#4B0082:#0000FF:#00FF00:#FFFF00:#FF7F00:#FF0000:%s>[Troll]</gradient>");
         PREFIXES.put("8b8tcore.prefix.donator6", "<gradient:#9145b0:#ceafcf:#9145b0:%s>[Ultra]</gradient>");
         PREFIXES.put("8b8tcore.prefix.donator5", "<gradient:#a18800:#ecc700:#a18800:%s>[Pro+]</gradient>");
         PREFIXES.put("8b8tcore.prefix.donator4", "<gradient:#ef9a98:#f7cac9:#ef9a98:%s>[Pro]</gradient>");
