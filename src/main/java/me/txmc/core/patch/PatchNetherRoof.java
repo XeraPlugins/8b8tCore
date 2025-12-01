4d8e52dac21c68ab75a9ee0b6587aef719419254
MrBeax31 <139056445+MrBeax31@users.noreply.github.com>
2025-12-01 14:37:37 +0100

    Add PatchNetherRoof.java that fixes some issues such as preventing the auto-kill.

--- src/main/java/me/txmc/core/patch/PatchNetherRoof.java
+++ src/main/java/me/txmc/core/patch/PatchNetherRoof.java
@@ -6,11 +6,10 @@ import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.BlockDamageEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.entity.Player;
 import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.block.BlockFace;
 
 public class PatchNetherRoof extends JavaPlugin implements Listener {
 
@@ -21,27 +20,29 @@ public class PatchNetherRoof extends JavaPlugin implements Listener {
         saveDefaultConfig();
         FileConfiguration cfg = getConfig();
         featureEnabled = cfg.getBoolean("ForceKillNetherRoof.enabled", true);
-
         Bukkit.getPluginManager().registerEvents(this, this);
     }
 
     @EventHandler
-    public void onBlockDamage(BlockDamageEvent event) {
+    public void onPlayerMove(PlayerMoveEvent event) {
         if (!featureEnabled) return;
 
+        if (event.getFrom().getBlockX() == event.getTo().getBlockX() &&
+            event.getFrom().getBlockY() == event.getTo().getBlockY() &&
+            event.getFrom().getBlockZ() == event.getTo().getBlockZ()) {
+            return;
+        }
+
         Player player = event.getPlayer();
-        Block block = event.getBlock();
-
         if (player.getWorld().getEnvironment() != World.Environment.NETHER) return;
-        if (block.getType() != Material.BEDROCK) return;
 
-        if (event.getBlockFace() != BlockFace.UP) return;
-
-        player.setHealth(0.0);
+        Block blockUnder = player.getLocation().subtract(0, 1, 0).getBlock();
+        if (blockUnder.getType() == Material.BEDROCK) {
+            player.setHealth(0.0);
+        }
     }
 }
 
-
-
 // If you want to enable/disable this, you may do it at almost the end of the file changing true for false.
+// THIS MIGHT NOT WORK AND NEED TO DO ANOTHER FIX (i'll handle that fix)
 // Last edited by MrBeax31  (https://github.com/MrBeax31)
