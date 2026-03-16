581cc5b4c07fe2cea269520234bc2f739c00ff22
libalpm64 <159192189+libalpm64@users.noreply.github.com>
2026-03-16 11:55:52 -0400

    Fix TPA
    Luminol handles teleports wierdly

--- src/main/java/me/txmc/core/tpa/commands/TPACommands.java
+++ src/main/java/me/txmc/core/tpa/commands/TPACommands.java
@@ -153,7 +153,7 @@ public class TPACommands {
             Location targetLoc = requested.getLocation();
             targetLoc.getWorld().getChunkAtAsyncUrgently(targetLoc.getBlock()).thenAccept(chunk -> {
                 if (requester.isOnline()) {
-                    requester.teleport(targetLoc, PlayerTeleportEvent.TeleportCause.PLUGIN);
+                    requester.teleportAsync(targetLoc, PlayerTeleportEvent.TeleportCause.PLUGIN);
                 }
             });
             sendPrefixedLocalizedMessage(requester, "tpa_teleporting");
@@ -171,7 +171,7 @@ public class TPACommands {
             Location targetLoc = requester.getLocation();
             targetLoc.getWorld().getChunkAtAsyncUrgently(targetLoc.getBlock()).thenAccept(chunk -> {
                 if (requested.isOnline()) {
-                    requested.teleport(targetLoc, PlayerTeleportEvent.TeleportCause.PLUGIN);
+                    requested.teleportAsync(targetLoc, PlayerTeleportEvent.TeleportCause.PLUGIN);
                 }
             });
             sendPrefixedLocalizedMessage(requester, "tpa_teleporting");
